dcd794fa71dee4e6011ec3ede66d74631286088e
Emma Paris <suginant@gmail.com>
2023-02-02 19:53:33 -0500

    Modified app.js and index.html added new files into css folder

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
 
-    res.send("Hello");
+    
+    res.sendFile(__dirname + "/index.html");
 });
+
+
+
+app.post("/", (req, res) => {
+
+    let simNum = Number(req.body.num1) + Number(req.body.num2);
+    console.log(simNum)
+
+    res.send(simNum.toString());
+});
+
 
 
 app.listen(3000, () => {
