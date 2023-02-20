c1c6cfc9f8881318267f22a33f22ca5582cbfc51
Emma Paris <suginant@gmail.com>
2023-02-19 21:09:05 -0500

    modified app.js file

--- app.js
+++ app.js
@@ -102,3 +102,78 @@ app.listen(port, () => {
 
     console.log("Server started on port " + port);
 });
+
+
+
+/**  const mongoPath = "C:\Program Files\MongoDB\Server\6.0\data\"; */
+
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', true); /** True */
+mongoose.connect("mongodb://localhost:27017/blogsDB", () => {
+  console.log("Connected to MongoDB");
+});
+
+// mongoose.connect("mongodb://localhost:27017/blogsDB", { useMongoClient: true });
+
+/**{ useFindAndModify: false }) or true */
+
+
+// mongoose.connect(Config.mongo_db_connection_string);
+
+// mongoose.set("strictQuery", false);
+// mongoose.connect(process.env.MONGO_URL, () => {
+//   console.log("Connected to MongoDB");
+// });
+
+const blogsSchema = new mongoose.Schema({
+    name: String,
+    blog: String
+});
+
+const Blog = mongoose.model("Blog", blogsSchema);
+
+const blog = new Blog({
+     name: "Blog 1",
+     blog: "lorem"
+}).save()
+
+
+
+// mongoose.conn.close();
+// .then(console.log)
+// .then(() => {
+//   return mongoose.conn.close();
+// }); 
+
+
+// Blog.remove({}, function(err) { 
+//   console.log('collection removed') 
+// });
+
+Blog.find((err, blogs) => {
+  if (err) {
+    console.log(err);
+  }
+  else {
+
+    // mongoose.connection.close(function () {
+    //   process.exit(0);
+    // });
+    // mongoose.connection.close(function () { console.log('Mongoose default connection closed'); });
+    // console.log(blogs);
+    // console.log(blogs.length)
+    
+    // mongoose.connection.close(function () { console. log('Mongoose default connection closed'); });
+    // mongoose.connection.close();
+  }
+});
+
+// Blog.find().then(doc => {
+//   console.log(doc);
+// }).then(() => {
+//   mongoose.connection.close();
+// });
+// blog.inspect();
+
+// blog.save();
