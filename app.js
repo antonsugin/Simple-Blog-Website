f5fa5618382f45e9521e28960a265719364a26f6
Emma Paris <suginant@gmail.com>
2023-02-20 21:29:02 -0500

    modified app.js and blog.ejs

--- app.js
+++ app.js
@@ -8,28 +8,49 @@ const _ = require('lodash');
 const bodyParser = require('body-parser')
 const ejs = require('ejs');
 
+const mongoose = require('mongoose');
 
+
+mongoose.set('strictQuery', true);
+// mongoose.set('strictQuery', false)
 const port = process.env.PORT || 3000;
 // const publicPath = path.join(__dirname, 'public/html');
 
+mongoose.connect("mongodb://localhost:27017/blogDB", () => {
+  console.log("Connected to MongoDB");
+});
 
-let blogItems = [];
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+// let blogItems = [];
 
 
 // app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 
-// router.use(express.urlencoded({ extended: false }));
+router.use(express.urlencoded({ extended: false }));
 
 router.get('/', (req, res) => {
     
-    res.render('blog', {blogs: blogItems});
-    
-    // res.sendFile(publicPath + '/index.html');
-
+  Blog.find({}, (err, blogs) => {
+    console.log(blogs);
+    res.render('blog', {
+      // content:"",
+      blogs: blogs
+    });
   });
+});
 
+
+
+  // {blogs: blogItems}
 router.get('/contact', (req, res) => {
 
     res.render('contact');
@@ -49,41 +70,59 @@ router.get('/compose', (req, res) => {
   });
 
 
-  router.get("/posts/:topic", (req, res) => {
+  router.get("/posts/:blogId", (req, res) => {
 
-    let reqTitle = _.lowerCase(req.params.topic);
+    // let reqTitle = _.lowerCase(req.params.topic);
+    const reqBlogId = req.params.blogId;
 
-    blogItems.forEach(post => {
+    Blog.findOne({_id: reqBlogId}, function(err, post){
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+    });
+
+    // blogItems.forEach(post => {
         
-        let postTitle = _.lowerCase(post.title);
+    //     let postTitle = _.lowerCase(post.title);
 
-        if (reqTitle === postTitle) {
+    //     if (reqTitle === postTitle) {
 
-          res.render('post', {
-            postTitle: post.title ,
-            PostText: post.text
-          });
+    //       res.render('post', {
+    //         postTitle: post.title ,
+    //         PostText: post.text
+    //       });
             
-        };
+    //     };
         
-    })
+    // })
 
   })
 
 
 app.post("/compose", (req, res) => {
 
-    const blogItemsObj = {
-        'title' : '',
-        'text' : ''
+  const blog = new Blog ({
+    title: req.body.title,
+    content: req.body.textarea
+  })
+
+  blog.save(function(err){
+    if (!err){
+        res.redirect("/");
     }
+  });
+    // const blogItemsObj = {
+    //     'title' : '',
+    //     'text' : ''
+    // }
 
-    blogItemsObj.title = req.body.title;
-    blogItemsObj.text = req.body.textarea;
+    // blogItemsObj.title = req.body.title;
+    // blogItemsObj.text = req.body.textarea;
     
-    blogItems.push(blogItemsObj);
+    // blogItems.push(blogItemsObj);
 
-    res.redirect('/');
+    // res.redirect('/');
 });
 
 app.post("/about", (req, res) => {
@@ -107,36 +146,16 @@ app.listen(port, () => {
 
 /**  const mongoPath = "C:\Program Files\MongoDB\Server\6.0\data\"; */
 
-const mongoose = require('mongoose');
-
-mongoose.set('strictQuery', true); /** True */
-mongoose.connect("mongodb://localhost:27017/blogsDB", () => {
-  console.log("Connected to MongoDB");
-});
-
-// mongoose.connect("mongodb://localhost:27017/blogsDB", { useMongoClient: true });
-
-/**{ useFindAndModify: false }) or true */
 
 
-// mongoose.connect(Config.mongo_db_connection_string);
 
-// mongoose.set("strictQuery", false);
-// mongoose.connect(process.env.MONGO_URL, () => {
-//   console.log("Connected to MongoDB");
-// });
 
-const blogsSchema = new mongoose.Schema({
-    name: String,
-    blog: String
-});
 
-const Blog = mongoose.model("Blog", blogsSchema);
 
-const blog = new Blog({
-     name: "Blog 1",
-     blog: "lorem"
-}).save()
+// const blog = new Blog({
+//      name: "Blog 1",
+//      blog: "lorem"
+// }).save()
 
 
 
@@ -151,23 +170,23 @@ const blog = new Blog({
 //   console.log('collection removed') 
 // });
 
-Blog.find((err, blogs) => {
-  if (err) {
-    console.log(err);
-  }
-  else {
+// Blog.find((err, blogs) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   else {
 
-    // mongoose.connection.close(function () {
-    //   process.exit(0);
-    // });
-    // mongoose.connection.close(function () { console.log('Mongoose default connection closed'); });
-    // console.log(blogs);
-    // console.log(blogs.length)
+//     // mongoose.connection.close(function () {
+//     //   process.exit(0);
+//     // });
+//     // mongoose.connection.close(function () { console.log('Mongoose default connection closed'); });
+//     // console.log(blogs);
+//     // console.log(blogs.length)
     
-    // mongoose.connection.close(function () { console. log('Mongoose default connection closed'); });
-    // mongoose.connection.close();
-  }
-});
+//     // mongoose.connection.close(function () { console. log('Mongoose default connection closed'); });
+//     // mongoose.connection.close();
+//   }
+// });
 
 // Blog.find().then(doc => {
 //   console.log(doc);
@@ -177,3 +196,25 @@ Blog.find((err, blogs) => {
 // blog.inspect();
 
 // blog.save();
+
+
+
+
+// <!-- <%  blogs.forEach(function(blog){ %>
+
+//   <section class="comment">
+//       <h4 class="comment__header"><%= blog.title %></h4>
+//       <p class="comment__txt"><%= blog.content.substring(0, 40) + " ..." %></p>
+//       <a href="posts/<%= blog._id %>" class="read-more" >Read More</a>
+//   </section>
+
+// <% }) %> -->
+  
+// <!-- <% for (let i = 0; i < blogs.length; i++) { %>
+
+//   <section class="comment">
+//       <h4 class="comment__header"><%= blogs[i].title %></h4>
+//       <p class="comment__txt"><%= blogs[i].text.substring(0, 40) + " ..." %></p>
+//       <a href="posts/<%= blogs[i].title %>" class="read-more" >Read More</a>
+//   </section>
+// <% } %> -->
